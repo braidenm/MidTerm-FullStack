09792cec177f27d218206f0b6875cfd2ea218a61
roguetycoon <michaelmurrell49@gmail.com>
2019-01-08 11:19:59 -0700

    MVC Dependencies

---
+++ MVCItMakesAVillage/src/main/java/com/itmakesavillage/MvcItMakesAVillageApplication.java
@@ -0,0 +1,14 @@
+package com.itmakesavillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcItMakesAVillageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcItMakesAVillageApplication.class, args);
+	}
+
+}
+
