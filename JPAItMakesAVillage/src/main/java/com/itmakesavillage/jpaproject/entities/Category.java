f3c963a2a4c43be8174860facedb1c74839ae554
Jimmy Fruchterman <jdfruchterman@gmail.com>
2019-01-09 14:09:16 -0700

    stubs and initial column mapping for user, project, category

--- JPAItMakesAVillage/src/main/java/com/itmakesavillage/jpaproject/entities/Category.java
+++ JPAItMakesAVillage/src/main/java/com/itmakesavillage/jpaproject/entities/Category.java
@@ -1,19 +1,15 @@
 package com.itmakesavillage.jpaproject.entities;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class User {
+public class Category {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
 	private int id;
-	@Column(name="user_name")
-	private String userName;
-	private String password;
-	private String email;
-	private String role;
+	private String name;
+	private String description;
 }
--- JPAItMakesAVillage/src/main/java/com/itmakesavillage/jpaproject/entities/Project.java
+++ JPAItMakesAVillage/src/main/java/com/itmakesavillage/jpaproject/entities/Project.java
@@ -4,20 +4,32 @@ import java.sql.Time;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Project {
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
 	private int id;
 	private String title;
 	private boolean active;
+	@Column(name="owner_id")
 	private int ownerId;
+	@Column(name="start_date")
 	private Date startDate;
+	@Column(name="end_date")
 	private Date endDate;
-	private String desription;
+	private String description;
+	
 	private Address address;
 	private Time time;
+	@Column(name="hours_needed")
 	private int hoursNeeded;
+	@Column(name="volunteer_needed")
 	private int volunteersNeeded;
 	private List<Volunteer> volunteers;
 	private List<Category> categories;
