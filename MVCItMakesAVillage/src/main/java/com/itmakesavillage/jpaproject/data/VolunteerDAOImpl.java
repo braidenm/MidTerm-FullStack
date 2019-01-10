7511063dcddb59428bfbb0f6a84149736a2e3253
roguetycoon <michaelmurrell49@gmail.com>
2019-01-10 10:09:53 -0700

    Volunteer DAO

--- MVCItMakesAVillage/src/main/java/com/itmakesavillage/jpaproject/data/VolunteerDAOImpl.java
+++ MVCItMakesAVillage/src/main/java/com/itmakesavillage/jpaproject/data/VolunteerDAOImpl.java
@@ -1,40 +1,63 @@
 package com.itmakesavillage.jpaproject.data;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
 import com.itmakesavillage.jpaproject.entities.Volunteer;
 
+@Transactional
+@Service
 public class VolunteerDAOImpl implements VolunteerDAO {
+
+	@PersistenceContext
+	EntityManager em;
 
 	@Override
 	public Volunteer findVolunteer(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Volunteer vol = em.find(Volunteer.class, id);
+		return vol;
 	}
 
 	@Override
 	public Volunteer createVolunteer(Volunteer volunteer) {
-		// TODO Auto-generated method stub
-		return null;
+		em.persist(volunteer);
+		return volunteer;
 	}
 
 	@Override
 	public Volunteer updateVolunteer(int id, Volunteer volunteer) {
-		// TODO Auto-generated method stub
-		return null;
+		Volunteer managed = em.find(Volunteer.class, id);
+		managed = volunteer;
+		return managed;
 	}
 
 	@Override
-	public Set<Volunteer> searchVolunteer(String Keyword) {
-		// TODO Auto-generated method stub
-		return null;
+	public Set<Volunteer> searchVolunteer(String keyword) {
+		keyword = "%" + keyword + "%";
+		keyword = keyword.replaceAll(" ", "% %");
+		String wordsarr[] = keyword.split(" ");
+		Set<Volunteer> vols = new HashSet<>();
+
+		for (String word : wordsarr) {
+			String qry = "SELECT vol FROM Volunteer vol WHERE vol.firstName LIKE :keyword OR vol.lastName LIKE :keyword";
+			vols.addAll(em.createQuery(qry, Volunteer.class).setParameter("keyword", word).getResultList());
+		}
+		return vols;
 	}
 
 	@Override
 	public List<Volunteer> getAllVolunteer() {
-		// TODO Auto-generated method stub
-		return null;
+		String qry = "SELECT vol from Volunteer vols";
+		List<Volunteer> vols = em.createQuery(qry, Volunteer.class).getResultList();
+		return vols;
 	}
 
 }
