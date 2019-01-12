baa1bfa9625ac9b6efc7a4e8fbc76003d90b5a66
Braiden Miller <braidenjamesmiller@gmail.com>
2019-01-12 14:07:52 -0700

    edit and create pages working

--- JPAItMakesAVillage/src/main/java/com/itmakesavillage/jpaproject/entities/Project.java
+++ JPAItMakesAVillage/src/main/java/com/itmakesavillage/jpaproject/entities/Project.java
@@ -1,11 +1,11 @@
 package com.itmakesavillage.jpaproject.entities;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -14,8 +14,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 @Entity
 public class Project {
@@ -55,7 +53,7 @@ public class Project {
 	@Column(name = "volunteers_needed")
 	private int volunteersNeeded;
 
-	@ManyToMany
+	@ManyToMany(fetch=FetchType.EAGER)
 	@JoinTable(name="project_volunteer", joinColumns=@JoinColumn(name="project_id"), inverseJoinColumns=@JoinColumn(name="volunteer_id"))
 	private List<Volunteer> volunteers;
 	
