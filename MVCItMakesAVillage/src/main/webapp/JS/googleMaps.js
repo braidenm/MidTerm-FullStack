ee3b7e0102a63a5f5dfe3ac0b0aaab8c7ba79579
roguetycoon <michaelmurrell49@gmail.com>
2019-01-15 11:26:02 -0700

    view project googlemaps stub

--- MVCItMakesAVillage/src/main/webapp/JS/googleMaps.js
+++ MVCItMakesAVillage/src/main/webapp/JS/googleMaps.js
@@ -1,65 +1,33 @@
+var geocoder;
+         var map;
 
-// Initialize and add the map
-    function initMap() {
-      var sd = {lat: 39.609012, lng: -104.902477};
-      var map = new google.maps.Map(
-          document.getElementById('map'), {zoom: 13, center: sd});
-      var marker = new google.maps.Marker({position: sd, map: map});
-    }
-    
-    
-    $(document).ready(function(){
-      // Add smooth scrolling to all links
-      $("a").on('click', function(event) {
+         function initialize() {
+           geocoder = new google.maps.Geocoder();
+           var latlng = new google.maps.LatLng(-34.397, 150.644);
+           var mapOptions = {
+             zoom: 15,
+             center: latlng,
+             mapTypeId: google.maps.MapTypeId.ROADMAP
+           }
+           map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+         }
 
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-          // Prevent default anchor click behavior
-          event.preventDefault();
-
-          // Store hash
-          var hash = this.hash;
-
-          // Using jQuery's animate() method to add smooth page scroll
-          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top
-          }, 800, function(){
-      
-            // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-          });
-        } // End if
-      });
-    });
-    
-    $(document).ready(function() {
-           var $rows = $('#table .searchRows');
-           $('#search').keyup(function() {
-             var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase().split(' ');
-
-             $rows.hide().filter(function() {
-               var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
-               var matchesSearch = true;
-               $(val).each(function(index, value) {
-                 matchesSearch = (!matchesSearch) ? false : ~text.indexOf(value);
-               });
-               return matchesSearch;
-             }).show();
+         function codeAddress() {
+           var address = document.getElementById("address").value;
+           geocoder.geocode( { 'address': address}, function(results, status) {
+             if (status == google.maps.GeocoderStatus.OK) {
+               map.setCenter(results[0].geometry.location);
+               var marker = new google.maps.Marker({
+                 map: map,
+                 position: results[0].geometry.location
+             });
+             } else {
+             alert("Geocode was not successful for the following reason: " + status);
+             }
            });
-    });
-    
-    $(document).ready(function() {
-        // Gets the video src from the data-src on each button
-         $(function() {
-                $(".video").click(function () {
-                  var theModal = $(this).data("target"),
-                  videoSRC = $(this).attr("data-video"),
-                  videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
-                  $(theModal + ' iframe').attr('src', videoSRCauto);
-                  $(theModal + ' button.close').click(function () {
-                    $(theModal + ' iframe').attr('src', videoSRC);
-                  });
-                });
-              });
-        });
+         }
+
+         window.onload = function() {
+             initialize();
+             codeAddress();
+         }
