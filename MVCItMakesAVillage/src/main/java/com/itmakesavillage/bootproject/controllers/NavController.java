05f3276d978b12a6be055b2c1369e78e7386fa09
Jimmy Fruchterman <jdfruchterman@gmail.com>
2019-01-15 16:18:40 -0700

    working on ticker

--- MVCItMakesAVillage/src/main/java/com/itmakesavillage/bootproject/controllers/NavController.java
+++ MVCItMakesAVillage/src/main/java/com/itmakesavillage/bootproject/controllers/NavController.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.itmakesavillage.bootproject.data.ProjectDAO;
+import com.itmakesavillage.bootproject.data.ProjectVolunteerDAO;
 import com.itmakesavillage.bootproject.data.VolunteerDAO;
 import com.itmakesavillage.jpaproject.entities.Category;
+import com.itmakesavillage.jpaproject.entities.Project;
+import com.itmakesavillage.jpaproject.entities.ProjectVolunteer;
 import com.itmakesavillage.jpaproject.entities.User;
 import com.itmakesavillage.jpaproject.entities.Volunteer;
 
@@ -22,12 +25,31 @@ public class NavController {
 	ProjectDAO pDAO;
 	@Autowired
 	VolunteerDAO vDAO;
+	@Autowired
+	ProjectVolunteerDAO pvDAO;
 	
 	@RequestMapping(path = "home.do", method = RequestMethod.GET)
 	public String goHome(HttpSession session) {
 		
 		List<Category> catList = pDAO.getAllCategories();
+		List<Project> projectList = pDAO.getAllProject();
+		int hoursContributed = 0, hoursPledged = 0, projects=0, volunteers = 0;
+		for (Project project : projectList) {
+			projects++;
+			volunteers += project.getVolunteers().size();
+		}
+		List<ProjectVolunteer> pvList = pvDAO.getAllProjectVolunteers();
+		for (ProjectVolunteer projectVolunteer : pvList) {
+			hoursContributed += projectVolunteer.getHoursActual();
+			hoursPledged += projectVolunteer.getHoursPledged();
+		}
+		
 		session.setAttribute("catList", catList);
+		session.setAttribute("hoursPledged", hoursPledged);
+		session.setAttribute("hoursContributed", hoursContributed);
+		session.setAttribute("projects", projects);
+		session.setAttribute("volunteers", volunteers);
+		
 		System.out.println(catList);
 		
 		return "index";
@@ -36,7 +58,24 @@ public class NavController {
 	public String gobasicpath(HttpSession session) {
 		
 		List<Category> catList = pDAO.getAllCategories();
+		List<Project> projectList = pDAO.getAllProject();
+		int hoursContributed = 0, hoursPledged = 0, projects=0, volunteers = 0;
+		for (Project project : projectList) {
+			projects++;
+			volunteers += project.getVolunteers().size();
+		}
+		List<ProjectVolunteer> pvList = pvDAO.getAllProjectVolunteers();
+		for (ProjectVolunteer projectVolunteer : pvList) {
+			hoursContributed += projectVolunteer.getHoursActual();
+			hoursPledged += projectVolunteer.getHoursPledged();
+		}
+		
 		session.setAttribute("catList", catList);
+		session.setAttribute("hoursPledged", hoursPledged);
+		session.setAttribute("hoursContributed", hoursContributed);
+		session.setAttribute("projects", projects);
+		session.setAttribute("volunteers", volunteers);
+		
 		System.out.println(catList);
 		
 		return "index";
