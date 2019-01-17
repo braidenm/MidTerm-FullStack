195bdebff8525b3302ee25149ab6ede476ebc265
Jimmy Fruchterman <jdfruchterman@gmail.com>
2019-01-17 14:24:17 -0700

    changes to upload to aWs

--- MVCItMakesAVillage/src/main/java/com/itmakesavillage/MvcItMakesAVillageApplication.java
+++ MVCItMakesAVillage/src/main/java/com/itmakesavillage/MvcItMakesAVillageApplication.java
@@ -2,9 +2,15 @@ package com.itmakesavillage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MvcItMakesAVillageApplication {
+public class MvcItMakesAVillageApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(MvcItMakesAVillageApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MvcItMakesAVillageApplication.class, args);
